b0413388ed74796b45910b996bbdcb7bf05ac554
gaurav <gparajuli.1997@gmail.com>
2024-03-02 13:41:43 +0545

    added pagination with htmx

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request
 from pprint import pprint
 import jinja_partials
 import feedparser
@@ -18,18 +18,37 @@ feeds = {
     }
 }
 
+# get entries
+for url, feed_ in feeds.items():
+    parsed_feed = feedparser.parse(url)
+    for entry in parsed_feed.entries:
+        if entry.link not in feed_['entries']:
+            feed_['entries'][entry.link] = entry
+
 app = Flask(__name__)
 jinja_partials.register_extensions(app) # will allow us to call jinja-partials functions from our template
+
+@app.route('/entries/<path:feed_url>')
+def render_feed_entries(feed_url:str):
+    try:
+        feed = feeds[feed_url]
+    except KeyError:
+        abort(404, message='feed not found')
+
+    page = int(request.args.get('page', 0))
+
+    return render_template(
+        'partials/entry_page.html',
+        entries = list(feed['entries'].values())[page*5:page*5+5],
+        href=feed_url,
+        page=page,
+        max_page = len(feed['entries']) // 5
+    )
+
 
 @app.route('/')
 @app.route('/feed/<path:feed_url>')
 def render_feed(feed_url:str = None):
-    for url, feed_ in feeds.items():
-        parsed_feed = feedparser.parse(url)
-        for entry in parsed_feed.entries:
-            if entry.link not in feed_['entries']:
-                feed_['entries'][entry.link] = entry
-
     if feed_url is None:
         feed = list(feeds.values())[0]
     else:
@@ -37,4 +56,4 @@ def render_feed(feed_url:str = None):
         feed = feeds[feed_url]
 
 
-    return render_template('feed.html', feed=feed['entries'].values(), feeds=feeds)
+    return render_template('feed.html', feed=feed, entries=feed['entries'].values(), feeds=feeds)
