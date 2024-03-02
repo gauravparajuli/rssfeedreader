3df9f41c385c920de14fb67626344c054c07c577
gaurav <gparajuli.1997@gmail.com>
2024-03-02 08:49:14 +0545

    endpoint added to render a single feed

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import jinja_partials
+import feedparser
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.get('/')
-def home():
-    return dict(message="it's working"), 200
+feed = {
+    'title': 'The Teclado Blog',
+    'href': 'https://blog.teclado.com/rss/',
+    'show_images': True,
+    'entries': {}
+}
+
+# @app.get('/')
+# def home():
+#     return dict(message="it's working"), 200
+
+def create_app():
+    app = Flask(__name__)
+    jinja_partials.register_extensions(app) # will allow us to call jinja-partials functions from our template
+
+    @app.route('/feed/')
+    def render_feed():
+        feed_url = feed['href']
+        parsed_feed = feedparser(feed_url)
+        for entry in parsed_feed.entries():
+            if entry.link not in feed['entries']:
+                feed['entries'][entry.link] = entry
+
+    return render_template('feed.html', feed=feed)
+
+    return app
